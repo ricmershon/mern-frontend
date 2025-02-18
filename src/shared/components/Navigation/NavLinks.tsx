4950cca3b63b5af7d1abe1d47f65e70e7bd7fa15
Ric Mershon <rmersh@ricmershon.com>
2025-02-18 12:37:21 -0500

    styling fixes

--- src/shared/components/Navigation/NavLinks.tsx
+++ src/shared/components/Navigation/NavLinks.tsx
@@ -1,32 +1,21 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 
 import './NavLinks.css';
 
 const NavLinks = (props) => {
     return (
-        <ul className="nav-links">
-            <li className="md:space-x-4 space-x-1">
-                <NavLink
-                    to="/"
-                    exact={true}
-                >
-                    ALL USERS
-                </NavLink>
-                <NavLink
-                    to="/u1/places"
-                >
-                    MY PLACES
-                </NavLink>
-                <NavLink
-                    to="/places/new"
-                >
-                    ADD PLACE
-                </NavLink>
-                <NavLink
-                    to="/auth"
-                >
-                    AUTHENTICATE
-                </NavLink>
+        <ul className="list-none m-0 p-0 w-full h-full flex flex-col justify-center items-center md:flex-row">
+            <li>
+                <NavLink to="/" exact>ALL USERS</NavLink>
+            </li>
+            <li>
+                <NavLink to="/u1/places">MY PLACES</NavLink>
+            </li>
+            <li>
+                <NavLink to="/places/new">ADD PLACE</NavLink>
+            </li>
+            <li>
+                <NavLink to="/auth">AUTHENTICATE</NavLink>
             </li>
         </ul>
     )
