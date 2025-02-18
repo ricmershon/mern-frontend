cafe382256c9bc30d3d0ace6ae4833bd90fdd320
Ric Mershon <rmersh@ricmershon.com>
2025-02-18 12:59:05 -0500

    react portal for side drawer

--- src/shared/components/Navigation/SideDrawer.tsx
+++ src/shared/components/Navigation/SideDrawer.tsx
@@ -1,11 +1,16 @@
 import { ReactNode } from "react";
+import { createPortal } from "react-dom";
+
+const sidedrawerContentElement = document.getElementById('sidedrawer-content')!;
 
 const SideDrawer = ({ children }: { children: ReactNode }) => {
-    return (
+    const content = (
         <aside className="fixed left-0 top-0 z-[100] h-screen w-[70%] bg-white shadow-[0_2px_8px_rgba(0,0,0,0.26)]">
             {children}
         </aside>
     )
+
+    return createPortal(content, sidedrawerContentElement);
 };
 
 export default SideDrawer;
