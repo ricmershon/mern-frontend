c29ce04503d1cf342a491986adc657d22b139f29
Ric Mershon <rmersh@ricmershon.com>
2025-02-28 13:02:42 -0500

    validators-typing

--- src/shared/utils/validators.ts
+++ src/shared/utils/validators.ts
@@ -13,38 +13,44 @@ const VALIDATOR_TYPE_FILE = 'FILE';
 
 export const ValidatorRequire = () => ({ type: VALIDATOR_TYPE_REQUIRE });
 export const ValidatorFile = () => ({ type: VALIDATOR_TYPE_FILE });
-export const ValidatorMinLength = (val) => ({
+export const ValidatorMinLength = (value: number) => ({
     type: VALIDATOR_TYPE_MINLENGTH,
-    val: val
+    value: value
 });
-export const ValidatorMaxLength = (val) => ({
+export const ValidatorMaxLength = (value: number) => ({
     type: VALIDATOR_TYPE_MAXLENGTH,
-    val: val
+    value: value
 });
-export const ValidatorMin = (val) => ({ type: VALIDATOR_TYPE_MIN, val: val });
-export const ValidatorMax = (val) => ({ type: VALIDATOR_TYPE_MAX, val: val });
+export const ValidatorMin = (value: string) => ({ type: VALIDATOR_TYPE_MIN, value: value });
+export const ValidatorMax = (value: string) => ({ type: VALIDATOR_TYPE_MAX, value: value });
 export const ValidatorEmail = () => ({ type: VALIDATOR_TYPE_EMAIL });
 
-export const Validate = (value, validators: InputValidators) => {
+export const Validate = (value: string | number, validators: InputValidators) => {
     let isValid = true;
     for (const validator of validators) {
-        if (validator.type === VALIDATOR_TYPE_REQUIRE) {
-            isValid = isValid && value.trim().length > 0;
-        }
-        if (validator.type === VALIDATOR_TYPE_MINLENGTH) {
-            isValid = isValid && value.trim().length >= validator.val;
-        }
-        if (validator.type === VALIDATOR_TYPE_MAXLENGTH) {
-            isValid = isValid && value.trim().length <= validator.val;
-        }
-        if (validator.type === VALIDATOR_TYPE_MIN) {
-            isValid = isValid && +value >= validator.val;
-        }
-        if (validator.type === VALIDATOR_TYPE_MAX) {
-            isValid = isValid && +value <= validator.val;
-        }
-        if (validator.type === VALIDATOR_TYPE_EMAIL) {
-            isValid = isValid && /^\S+@\S+\.\S+$/.test(value);
+        if (typeof value === 'string') {
+            if (typeof validator.value === 'number') {
+                switch (validator.type) {
+                    case VALIDATOR_TYPE_MINLENGTH:
+                        isValid = isValid && value.trim().length >= validator.value;
+                        return;
+                    case VALIDATOR_TYPE_MAXLENGTH:
+                        isValid = isValid && value.trim().length <= validator.value;
+                        return;
+                    case VALIDATOR_TYPE_MIN:
+                        isValid = isValid && +value >= validator.value;
+                        return;
+                    case VALIDATOR_TYPE_MAX:
+                        isValid = isValid && +value <= validator.value;
+                }
+            } else {
+                if (validator.type === VALIDATOR_TYPE_REQUIRE) {
+                    isValid = isValid && value.trim().length > 0;
+                }
+                if (validator.type === VALIDATOR_TYPE_EMAIL) {
+                    isValid = isValid && /^\S+@\S+\.\S+$/.test(value);
+                }
+            }
         }
     }
     return isValid;
